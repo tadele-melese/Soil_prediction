33f1c6b054494af7c09ea9d6f41ce2f6787e32b6
Tadele Melese <tadelemelese21m@gmail.com>
2025-08-26 00:22:52 +0900

    Updated data

--- test.py
+++ test.py
@@ -1,53 +1,52 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "f703b676",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Import required libraries\n",
-    "import pandas as pd \n",
-    "import streamlit as st"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "18a52c51",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "87989969",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "ML_Drought",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.23"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import streamlit as st
+import pandas as pd
+
+import streamlit as st
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #c3c3c3;  /* light gray */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown("<h1 style='color:red'>Hello</h1>", unsafe_allow_html=True)
+# Title
+st.title("Demonstration Dashboard")
+st.write("This dashboards visuallize soil parameters, analysis and prediction")
+tab1,tab2,tab3,tab4 = st.tabs(['Basic Statistcs', 'Visualization', 'Model Training', 'Performance'])
+# Load the dataset
+data =pd.read_csv("simulated_soil_data.csv")
+data
+with tab1:
+    st.title('Basic Statisics Information')
+    st.markdown("""
+                # 'Description',
+                *Inferential*
+                """)
+with tab2:
+    st.title('Visualzation section')
+    st.dataframe(data)
+    st.scatter_chart(data, x='EC',y='Soil_pH')
+
+with tab3:
+    st.bar_chart(data[['EC','Soil_pH', 'Organic_Carbon', 'Clay_Content']])
+with tab4:
+    st.write('Model performance will be displayed here!  Stay tuned!!!!!!!!!!!!!!!!')
+    st.bar_chart(data, x="category", y="sales")
+
+st.dataframe(data)
+
+st.line_chart(data['EC'])
+st.bar_chart(data[['EC','Soil_pH', 'Organic_Carbon', 'Clay_Content']])
+st.scatter_chart(data, x='EC',y='Soil_pH')
+# to show many colomns from the table
+# Create side bar
+
+st.sidebar.title("Data Filter Bar")
+ph_min, ph_max = st.sidebar.slider('Soil pH range', float(data['Soil_pH'].min()), float(data['Soil_pH'].max()), (1.0, 9.0))
+
